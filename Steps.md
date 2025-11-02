c9d223d628492859f55e511f23b1c8f06bd3d2e6
Devansh <devanshprataps6@gmail.com>
2025-11-02 22:52:49 +0530

    Updated logger.py and exception.py for logs creation and exception handing respectively

--- Steps.md
+++ Steps.md
@@ -2,3 +2,5 @@
     - Network-Data,networksecurity,notebooks,venv,requirements.txt,.env,.gitignore
 
 2. `setup.py`
+3. `networksecurity/logging/logger.py`
+4. `networksecurity/exception/exception.py`
---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,15 @@
+import sys
+from networksecurity.logging import logger
+
+class NetworkSecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info() # (type, value, traceback)
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        self.fun_name = exc_tb.tb_frame.f_code.co_name
+    
+    def __str__(self):
+        return f"Error occured in python script name {self.file_name} line number {self.lineno} error message {str(self.error_message)}, fun_name = {self.fun_name}"
+        
