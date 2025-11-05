5799d51080e126459ef28ee4edf838eb40ef0fe0
Devansh <devanshprataps6@gmail.com>
2025-11-05 22:07:21 +0530

    Fixed foler naming of created log folder

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%M_%H_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE[:-4])
 
 os.makedirs(logs_path,exist_ok=True)
 
