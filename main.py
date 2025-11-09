96dcd1015a2b9084d2de60b26e3e201a2a6aede6
Devansh <devanshprataps6@gmail.com>
2025-11-09 19:58:08 +0530

    Created Data Transformation Pipeline

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -21,5 +22,11 @@ if __name__ == "__main__":
         dva = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(dva)
+        dtc = DataTransformationConfig(tpc)
+        data_transformation = DataTransformation(dva,dtc)
+        logging.info("Initiate the Data Transformation")
+        dta = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(dta)
     except Exception as e:
         raise NetworkSecurityException(e,sys)    
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -34,3 +34,20 @@ DATA_VALIDATION_VALID_DIR = "validated"
 DATA_VALIDATION_INVALID_DIR = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "report.yaml"
+
+"""
+Data Transformation related constant
+"""
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+DATA_TRANSFORMATION_DIR_NAME = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR = "transformed_object"
+
+## KNN Imputer for replacing NaN values.
+DATA_TRANSFORMATION_IMPUTER_PARAMS = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH = "test.npy"
