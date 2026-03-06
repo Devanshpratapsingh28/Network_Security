e2b98258af17db0826d24f51df29fbabb2c042a1
Devansh Pratap Singh <160342233+Devanshpratapsingh28@users.noreply.github.com>
2026-03-06 16:41:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,36 +15,6 @@ warnings.filterwarnings(
     message="pkg_resources is deprecated as an API"
 )
 
-# if __name__ == "__main__":
-#     try:
-#         tpc = TrainingPipelineConfig()
-#         dic = DataIngestionConfig(tpc)
-#         data_ingestion = DataIngestion(dic)
-#         logging.info("Initiate the Data Ingestion")
-#         dia = data_ingestion.initiate_data_ingestion()
-#         logging.info("Data Ingestion Completed")
-#         print(dia)
-#         #dia_dummy = DataIngestionArtifact(trained_file_path="/Users/devanshpratap28/Downloads/My Folder/MLOPS_KRISHNAIK/Network_Security/Artifacts/12_22_2025_19_58_13/data_ingestion/ingested/train.csv",test_file_path="/Users/devanshpratap28/Downloads/My Folder/MLOPS_KRISHNAIK/Network_Security/Artifacts/12_22_2025_19_58_13/data_ingestion/ingested/test.csv")
-#         dvc = DataValidationConfig(tpc)
-#         data_validation = DataValidation(dia,dvc)
-#         logging.info("Initiate the Data Validation")
-#         dva = data_validation.initiate_data_validation()
-#         logging.info("Data Validation Completed")
-#         print(dva)
-#         dtc = DataTransformationConfig(tpc)
-#         data_transformation = DataTransformation(dva,dtc)
-#         logging.info("Initiate the Data Transformation")
-#         dta = data_transformation.initiate_data_transformation()
-#         logging.info("Data Transformation Completed")
-#         print(dta)
-#         logging.info("Model Training Started")
-#         mtc = ModelTrainerConfig(tpc)
-#         model_trainer = ModelTrainer(dta, mtc)
-#         mta = model_trainer.initiate_model_trainer()
-#         logging.info("Model Training Artifact Created")
-#     except Exception as e:
-#         raise NetworkSecurityException(e,sys)    
-
 if __name__ == "__main__":
     try:
         training_pipeline = TrainingPipeline()
