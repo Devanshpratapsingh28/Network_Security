01c6f793dd08391b8bde78822f14288312978540
Devansh <devanshprataps6@gmail.com>
2026-07-25 10:52:49 +0530

    fixed training_pipeline

--- networksecurity/pipelines/training_pipeline.py
+++ networksecurity/pipelines/training_pipeline.py
@@ -101,9 +101,14 @@ class TrainingPipeline:
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-
-            self.sync_artifact_dir_to_s3()
-            self.sync_saved_model_dir_to_s3()
+            
+            try :
+                self.sync_artifact_dir_to_s3()
+                self.sync_saved_model_dir_to_s3()
+            
+            except Exception as e:
+                logging.warning(f"S3 sync skipped: {e}")
+            
 
             return model_trainer_artifact
         except Exception as e:
